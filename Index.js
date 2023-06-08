92f58198c8fd79bba38def68ba6b44cc47aa9f9d
Ian Ndungu <ianndungu06@gmail.com>
2023-06-08 13:27:40 +0300

    Done with project

---
+++ Index.js
@@ -0,0 +1,19 @@
+let a=3;
+let b=5;
+
+console.log(a+b); //prints 8
+
+const c=10;
+const d=15;
+
+console.log(c+d); //output 25
+
+a=7;
+b=10;
+
+console.log(a+b); //output 17
+
+let f=20;
+let g=15;
+
+console.log(f+g);
---
+++ main.js
@@ -0,0 +1,24 @@
+let students = [
+    {
+      name: "Ian Ndungu",
+      age: 22,
+      grade: "B"
+    },
+    {
+      name: "Lionel Kasambeli",
+      age: 25,
+      grade: "B+",
+    },
+    {
+      name: "Adrian Munandi",
+      age: 24,
+      grade: "A"
+    },
+    {
+      name: "Emmanuel Marial",  
+      age: 27,
+      grade: "B-"
+    }
+  ];
+
+  console.log(students)
